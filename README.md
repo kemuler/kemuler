fcd1f63d7bbcb3c32df48d581b5178b4fdbfe5bc
Multirious <Multirious@outlook.com>
2023-07-25 14:44:30 +0700

    stuff

---
+++ README.md
@@ -0,0 +1,28 @@
+Welcome to an experimental input simulation crate, `kemuler`!
+
+This crate offers a different kind of high level input simulator framework,
+utilizing Rust’s type system to its full potential (or just over-engineered)
+instead of the usual `key_down(Key)`.
+Why?
+Combinators!
+
+Current features:
+- Multiple backends support (called `Simulator` in here).
+  Built-ins:
+  - Enigo (The crate that helped me make this crate and it is cross-platform.)
+  - (WIP, seperated branch) Windows (support for Window's DirectX/DirectInput something something game something something)
+- Combinator
+
+Some drawbacks:
+- Combinator currently can only combine for the same simulator;
+  any simulator combinator is currently on a separated branch.
+- Only a few amount of combinators is present.
+  If you've got some more useful combinator, please submit an issue on `GitHub`!
+
+# Why is it experimental?
+This crate doesn't really solve any problem other than just easier to do
+input simulation (maybe that will be the selling point).
+It's also fresh out so some breaking changes might occur in the future.
+This crate is just me playing around with the language.
+We'll see if this crate will actually get any traction.
+If it did, then experimental tag will be remove lol.
--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,4 @@
-//! # Kemuler
-//!
-//! Welcome to an experimental input simulation crate, `kemuler`!
-//!
-//! This crate offers a different kind of high level input simulator framework,
-//! utilizing Rust’s type system to its full potential (or just over-engineered)
-//! instead of the usual `key_down(Key)`.
-//! Why?
-//! Combinators!
-//!
-//! Current features:
-//! - Multiple backends support (called `Simulator` in here).
-//!   Built-ins (that's one thing, i know, wip :V):
-//!   - Enigo (The crate that helped me make this crate and it is cross-platform.)
-//!   - (WIP, seperated branch) Windows (support for Window's DirectX/DirectInput something something game something something)
-//! - Combinator
-//!
-//! Some drawbacks:
-//! - Combinator currently can only combine for the same simulator;
-//!   any simulator combinator is currently on a separated branch.
-//! - Only a few amount of combinators is present.
-//!   If you've got some more useful combinator, please submit an issue on `GitHub`!
-//!
+#![doc = include_str!("../README.md")]
 //! # Examples
 //! All these examples requires the feature "enigo".
 //!
@@ -94,14 +72,6 @@
 //!     .repeat(20)
 //!     .run_with(&mut enigo);
 //! ```
-//!
-//! # Why is it experimental?
-//! This crate doesn't really solve any problem other than just easier to do
-//! input simulation (maybe that will be the selling point).
-//! It's also fresh out so some breaking changes might occur in the future.
-//! This crate is just me playing around with the language.
-//! We'll see if this crate will actually get any traction.
-//! If it did, then experimental tag will be remove lol.
 #![cfg_attr(all(doc, CHANNEL_NIGHTLY), feature(doc_auto_cfg))]
 
 pub mod combinator;
