1a66170558bd893a85e78efd2250848b09bf04f6
Multirious <multirious@outlook.com>
2023-08-07 22:23:35 +0700

    update doc

--- build_doc.sh
+++ build_doc.sh
@@ -1,4 +1,4 @@
 cargo +nightly doc --no-deps --target-dir target --features "spin_sleep"
 rm -rf ./docs
-echo "<meta http-equiv=\"refresh\" content=\"0; url=build_wheel\">" > target/doc/index.html
+echo "<meta http-equiv=\"refresh\" content=\"0; url=kemuler\">" > target/doc/index.html
 cp -r target/doc ./docs
