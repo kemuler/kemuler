ca9fd188045b9305c36b7f7d02ef479ebbf1de46
Multirious <multirious@outlook.com>
2023-08-14 07:26:40 +0700

    impl Simulate for tuple (only for 2 rn)

--- src/simulator.rs
+++ src/simulator.rs
@@ -6,3 +6,15 @@ pub trait Simulate<S> {
     /// Simulate this simulatable
     fn simulate(&mut self, simulatable: S);
 }
+
+impl<S, Smlt0, Smlt1> Simulate<S> for (&mut Smlt0, &mut Smlt1)
+where
+    S: Clone,
+    Smlt0: Simulate<S>,
+    Smlt1: Simulate<S>,
+{
+    fn simulate(&mut self, simulatable: S) {
+        self.0.simulate(simulatable.clone());
+        self.1.simulate(simulatable);
+    }
+}
